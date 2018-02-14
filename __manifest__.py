983a37c4ff53a7ffc0657fc98bf362e9a579d3f6
Gabriel Pabon <gabriel.pabon@github.com>
2018-02-13 23:12:59 -0500

    actualizacion 1 codigos

---
+++ __manifest__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Directorio Empleados y Partes PROMA',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'sequence': 79,
+    'summary': 'Jobs, Departments, Employees Details',
+    'description': """
+Human Resources Management
+==========================
+
+This application enables you to manage important aspects of your company's staff and other details such as their skills, contacts, working time...
+
+
+You can manage:
+---------------
+* Employees and hierarchies : You can define your employee with User and display hierarchies
+* HR Departments
+* HR Jobs
+    """,
+    'website': 'https://www.odoo.com/',
+    'images': [
+        'images/hr_department.jpeg',
+        'images/hr_employee.jpeg',
+        'images/hr_job_position.jpeg',
+        'static/src/img/default_image.png',
+    ],
+    'depends': [
+        'base_setup',
+        'mail',
+        'resource',
+        'web_kanban',
+    ],
+    'data': [
+        'security/hr_security.xml',
+        'security/ir.model.access.csv',
+        'views/hr_views.xml',
+        'views/hr_templates.xml',
+        'data/hr_data.xml',
+    ],
+    'demo': [
+        'data/hr_demo.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [],
+}
