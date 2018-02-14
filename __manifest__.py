e0fc67859ffa531451a9744b0d6ae7f8248f8830
Gabriel Pabon <gabriel.pabon@github.com>
2018-02-13 23:35:24 -0500

    xD

--- __manifest__.py
+++ __manifest__.py
@@ -38,7 +38,7 @@ You can manage:
         'security/ir.model.access.csv',
         'views/hr_views.xml',
         'views/hr_templates.xml',
-        'views/motores.xml'
+        'views/motores.xml',
         'data/hr_data.xml',
     ],
     'demo': [
